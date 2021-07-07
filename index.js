2477d693e7fc16d3ee13475358c45a2153707e69
Haripriya Baskaran <haripriyabaskaran@Haripriya-baskarans-MacBook-Pro.local>
2021-07-07 23:52:26 +0530

    Refactor: routenames, update value logic, cosmetic changes

--- index.js
+++ index.js
@@ -3,117 +3,119 @@ NOTE: The commented codes are of Linkedlist implementation
 Current code contains Red-Black tree implementation.
 */
 
-const express = require('express');
+const express = require("express");
 const bodyparser = require("body-parser");
-const RbTree = require("functional-red-black-tree")
 
 const app = express();
 
-//initialise middlewares
-const middlewares = require("./middlewares");
-app.use(bodyparser.urlencoded({extended:false}));
+//initialise services
+const services = require("./services");
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 // @type    -   GET
-// @route   -   /
+// @route   -   /map/display
 // @desc    -   Display the custom hashmap
 // @access  -   PUBLIC
-app.get("/",(req,res)=>{
-    const table = middlewares.showTable();
-    let jsonObj = [];
-    table.forEach(tree=>{
-        // console.log(tree.keys);
-        // console.log(tree.values);
-        const keys = tree.keys;
-        const values = tree.values;
-        for(let i=0;i<keys.length;i++){
-            const obj={
-                key: keys[i],
-                value: values[i]
-            };
-            jsonObj.push(obj);
-        }
-    });
+app.get("/map/display", (req, res) => {
+  const table = services.showTable();
+  let jsonObj = [];
+  table.forEach((tree) => {
+    // console.log(tree.keys);
+    // console.log(tree.values);
+    const keys = tree.keys;
+    const values = tree.values;
+    for (let i = 0; i < keys.length; i++) {
+      const obj = {
+        key: keys[i],
+        value: values[i],
+      };
+      jsonObj.push(obj);
+    }
+  });
 
-    // table.forEach(element => {
-    //     const obj = {
-    //         key: element.key,
-    //         value: element.value
-    //     };
-    //     if(element.next !=null){
-    //         while(element!=null){
-    //             const obj = {
-    //                 key: element.key,
-    //                 value: element.value
-    //             };
-    //             jsonObj.push(obj);
-    //             element = element.next;
-    //         }
-    //     }
-    //     else {
-    //         jsonObj.push(obj);
-    //     }
-    // });
-    return res.json({table:jsonObj});
+  // table.forEach(element => {
+  //     const obj = {
+  //         key: element.key,
+  //         value: element.value
+  //     };
+  //     if(element.next !=null){
+  //         while(element!=null){
+  //             const obj = {
+  //                 key: element.key,
+  //                 value: element.value
+  //             };
+  //             jsonObj.push(obj);
+  //             element = element.next;
+  //         }
+  //     }
+  //     else {
+  //         jsonObj.push(obj);
+  //     }
+  // });
+  return res.json({ table: jsonObj });
 });
 
 //Function to handle post and put requests
-function updateorcreate(req,res){
-    const key = req.body.key;
-    const value = req.body.value;
-    middlewares.insertion(key,value)
-    .then(result=>{
-        return res.status(201).json({success:result});
-    })
-    .catch(err=>console.log(err));
+async function updateorcreate(req, res) {
+  const key = req.body.key;
+  const value = req.body.value;
+  let result = await services.insertion(key, value);
+  return res.status(201).json({ success: result });
 }
 
 // @type    -   POST
-// @route   -   /add
+// @route   -   /map
 // @desc    -   insertion of a key value pair, if already present then update
 // @access  -   PUBLIC
-app.post("/add",updateorcreate);
+app.post("/map", (req, res) => {
+  updateorcreate(req, res).catch((err) => console.log(`Error: ${err}`));
+});
 
 // @type    -   PUT
-// @route   -   /add
+// @route   -   /map
 // @desc    -   Updation of a key value pair
 // @access  -   PUBLIC
-app.put("/add",updateorcreate);
+app.put("/map", (req, res) => {
+  updateorcreate(req, res).catch((err) => console.log(`Error: ${err}`));
+});
 
 // @type    -   GET
-// @route   -   /get
+// @route   -   /map/:key
 // @desc    -   read the value of a key
 // @access  -   PUBLIC
-app.get("/get",(req,res)=>{
-    const key = req.body.key;
-    middlewares.getvalueofkey(key)
-    .then(result=>{
-        if(result!=null){
-            return res.json({success:`Key: ${key}, Value: ${result}`});
-        } else{
-            return res.status(404).json({error:"Key not found!"});
-        }
-    })
-    .catch(err=>console.log(err));
+app.get("/map/:key", async (req, res) => {
+  try {
+    const key = req.params.key;
+    let result = await services.getvalueofkey(key);
+    if (!result) {
+      return res.status(404).json({ error: "Key not found!" });
+    } else {
+      return res.json({ success: `Key: ${key}, Value: ${result}` });
+    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 });
 
 // @type    -   DELETE
-// @route   -   /delete
+// @route   -   /map/:key
 // @desc    -   delete a key node
 // @access  -   PUBLIC
-app.delete("/delete",(req,res)=>{
-    const key = req.body.key;
-    middlewares.deletekeyvaluepair(key)
-    .then(result=>{
-        if(result==true){
-            return res.json({success:`${key} deleted successfully`});
-        } else{
-            return res.status(404).json({error:"Key not found!"});
-        }
-    })
-    .catch(err=>console.log(err));
+app.delete("/map/:key", async (req, res) => {
+  try {
+    const key = req.params.key;
+    let result = await services.deletekeyvaluepair(key);
+    if (result == true) {
+      return res.json({ success: `${key} deleted successfully` });
+    } else {
+      return res.status(404).json({ error: "Key not found!" });
+    }
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
 });
 
 const port = process.env.port || 5000;
 
-app.listen(port,()=> console.log(`Server running at ${port}`));
+app.listen(port, () => console.log(`Server running at ${port}`));
---
+++ services.js
@@ -0,0 +1,142 @@
+const Entry = require("./models/Entry");
+const createRBTree = require("functional-red-black-tree");
+
+var table = [];
+var cache = [];
+var mapcapacity = 16;
+var size = 0;
+
+function showTable() {
+  return cache;
+}
+
+function hash(key) {
+  let ind = 0;
+  for (let i = 0; i < key.length; i++) {
+    ind += key.charCodeAt(i);
+  }
+  ind = ind % mapcapacity;
+  return ind;
+}
+
+function rehash() {
+    mapcapacity = 2 * mapcapacity;
+    let temp = table;
+    table = [];
+    size = 0;
+    temp.forEach((node) => {
+      const keys = node.keys;
+      const values = node.values;
+      for (let i = 0; i < keys.length; i++) {
+        insertion(keys[i], values[i]);
+      }
+
+      // while(node!=null){
+      //     insertion(node.key,node.value);
+      //     node=node.next;
+      // }
+    });
+    cache = table;
+}
+
+function insertion(key, value) {
+    if (size == mapcapacity * 8) {
+        rehash();
+    }
+  let index = hash(key);
+  // var newnode = new Entry(key,value,null);
+
+  if (cache[index] == null) {
+    var rbTree = createRBTree();
+    cache[index] = rbTree.insert(key, value);
+    table[index] = rbTree.insert(key, value);
+    size++;
+  } else {
+    let currentTree = cache[index];
+    if (currentTree.get(key) == null) {
+      currentTree = currentTree.insert(key, value);
+      size++;
+    } else {
+      var iter = currentTree.find(key);
+      currentTree = iter.update(value);
+      cache[index] = currentTree;
+      table[index] = currentTree;
+      return `${key} was already present, Value updated!`;
+    }
+    cache[index] = currentTree;
+    table[index] = currentTree;
+  }
+  // if(table[index] == null){
+  //     table[index] = newnode;
+  //     size++;
+  // } else {
+  //     let previousNode = null;
+  //     let currentNode = table[index];
+  //     while(currentNode!=null){
+  //         if(currentNode.key == key){
+  //             currentNode.value = value;
+  //             return `${key} was already present, Value updated!`;
+  //         }
+  //         previousNode = currentNode;
+  //         currentNode = currentNode.next;
+  //     }
+  //     if(previousNode != null && currentNode == null){
+  //         size++;
+  //         previousNode.next = newnode;
+  //     }
+  // }
+  console.log(`${index}  ${size}`);
+  return `${key} - ${value} inserted successfully`;
+}
+
+function getvalueofkey(key) {
+  const index = hash(key);
+  let listHead = cache[index];
+  if (listHead != null) {
+    return listHead.get(key);
+
+    // while(listHead!=null){
+    //     if(listHead.key == key){
+    //         return listHead.value;
+    //     }
+    //     listHead = listHead.next;
+    // }
+  }
+  return null;
+}
+
+function deletekeyvaluepair(key) {
+  const index = hash(key);
+  let previousNode = null;
+  let listHead = cache[index];
+  if (listHead) {
+    if (listHead.get(key)) {
+      cache[index] = listHead.remove(key);
+      table[index] = listHead.remove(key);
+      return true;
+    }
+    // while(listHead!=null){
+    //     if(listHead.key == key){
+    //         if(previousNode == null){
+    //             listHead = listHead.next;
+    //             table[index] = listHead;
+    //             return true;
+    //         } else{
+    //             previousNode.next = listHead.next;
+    //             return true;
+    //         }
+    //     }
+    //     previousNode = listHead;
+    //     listHead = listHead.next;
+    // }
+  }
+  return false;
+}
+
+module.exports = {
+  showTable,
+  hash,
+  insertion,
+  deletekeyvaluepair,
+  getvalueofkey,
+};
